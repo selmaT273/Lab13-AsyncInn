b56df0eaf9a6e54c0c1981eb592a0cddfe155a9b
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-20 16:06:18 -0500

    connection string stuff

---
+++ Lab13-AsyncInn/Data/HotelDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+
+
+namespace Lab13_AsyncInn.Data
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
